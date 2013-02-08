1b373282f90bd890fcfa1ff4ff0ba48019f94893
Jubi Dition <yubi4dition@googlemail.com>
2013-02-08 17:45:18 +0100

    wrong title

--- application/views/includes/header.php
+++ application/views/includes/header.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
   <meta charset="utf-8">
-  <title>Simple Gallery</title>
+  <title>Juballery</title>
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <!-- Le styles -->
   <link href="../assets/css/bootstrap.css" rel="stylesheet">
